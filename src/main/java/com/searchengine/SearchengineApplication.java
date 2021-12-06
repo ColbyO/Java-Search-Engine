bed8eeeb63f2c023e91094923e5040711f70ba38
ColbyO <71828390+ColbyO@users.noreply.github.com>
2021-12-06 19:02:42 -0330

    fixed failed to run bug

--- src/main/java/com/searchengine/SearchengineApplication.java
+++ src/main/java/com/searchengine/SearchengineApplication.java
@@ -2,10 +2,8 @@ package com.searchengine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories
 public class SearchengineApplication {
 
 	public static void main(String[] args) {
