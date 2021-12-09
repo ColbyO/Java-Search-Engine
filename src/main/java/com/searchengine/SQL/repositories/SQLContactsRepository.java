4d61ae9ec6964930cd039f4e6c914b18789d8ed3
ColbyO <71828390+ColbyO@users.noreply.github.com>
2021-12-09 17:24:12 -0330

    fixed two datasources bug

--- src/main/java/com/searchengine/SQL/repositories/SQLContactsRepository.java
+++ src/main/java/com/searchengine/SQL/repositories/SQLContactsRepository.java
@@ -4,14 +4,14 @@ import java.util.List;
 
 import com.searchengine.SQL.models.SQLContacts;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RepositoryRestResource(collectionResourceRel = "contacts", path = "contacts")
-public interface SQLContactsRepository extends PagingAndSortingRepository<SQLContacts, Long> {
+public interface SQLContactsRepository extends JpaRepository<SQLContacts, Long> {
     List<SQLContacts> findByFirstName(@Param("firstName") String firstName);
     List<SQLContacts> findByLastName(@Param("lastName") String lastName);
     List<SQLContacts> findByEmail(@Param("email") String email);
